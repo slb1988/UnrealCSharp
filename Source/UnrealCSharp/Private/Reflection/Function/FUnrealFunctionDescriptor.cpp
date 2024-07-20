5bf35ce4a73486c5b41d4b82e50a7c7c950084e6
crazytuzi <liuxiangcode@qq.com>
2024-07-20 19:34:36 +0800

    t

--- Source/UnrealCSharp/Private/Reflection/Function/FUnrealFunctionDescriptor.cpp
+++ Source/UnrealCSharp/Private/Reflection/Function/FUnrealFunctionDescriptor.cpp
@@ -66,7 +66,7 @@
 			{ \
 				MonoObject* Value = nullptr; \
 				OutPropertyDescriptor->Get(OutPropertyDescriptor->ContainerPtrToValuePtr<void>(Params), \
-				                           reinterpret_cast<void**>(&Value), false); \
+				                           reinterpret_cast<void**>(&Value), true); \
 				ARRAY_SET(MonoObjectArray, MonoObject*, Index, Value); \
 			} \
 		} \
